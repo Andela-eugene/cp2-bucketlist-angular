b823e380d3cc30ad4a73a703abb17f85aa924a40
Andela-eugene <eugene.liyai@andela.com>
2017-05-14 11:08:51 +0300

    [Feature] style search modal

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -14,17 +14,24 @@ export class NavbarComponent implements OnInit {
   errorMessage: any;
   searchTerm: any;
   constructor(public router: Router,
-              private _bucketlistService: BucketlistService) { }
+              private _bucketlistService: BucketlistService, private _router: Router) { }
 
   ngOnInit() {
     this.user = localStorage.getItem('user');
   }
 
   searchBucketlist() {
+    this.allSearchBucketlists = []
     this._bucketlistService.searchBucketlist(this.searchTerm).subscribe(
         bucketlists => {this.allSearchBucketlists = bucketlists;
       },
       error => this.errorMessage = <any> error
     );
+  }
+  redirectToItems(bucket_id) {
+    this._router.navigate(['/bucketlist', bucket_id]);
+  }
+  redirectToBucket(bucket_id) {
+    this._router.navigate(['/bucket_update', bucket_id]);
   }
 }
